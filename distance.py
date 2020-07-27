0df4e3b5240ca75e189764c3b5d734520e33143b
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2020-07-27 15:28:52 -0400

    Enforce symmetry

--- distance.py
+++ distance.py
@@ -1,12 +1,14 @@
 import lz4.frame as compressor # Use lzma for shorter array
 
-def ncd(x, y):
+def ncd(x, y, symmetric = True):
     """
         Computes distance between x and y
 
         Args:
             x (numpy array): Object 1
             y (numpy array): Object 2
+
+            symmetric (bool): Enforce symmetry (slower)
 
         Returns:
             Distance between objects
@@ -18,6 +20,9 @@ def ncd(x, y):
     x_compession = len(compressor.compress(x_str))
     y_compession = len(compressor.compress(y_str))
     xy_compession = len(compressor.compress(x_str + y_str))
+
+    # Ensure symmetry for fast compression
+    xy_compession = min(xy_compession, len(compressor.compress(y_str + x_str)))
 
     # Compute distance
     return (xy_compession - min(x_compession, y_compession)) / max(x_compession, y_compession)
